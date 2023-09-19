2c873fe5c708ce353a0541aa2df2bde842ad0ca5
unknown <Baxtiyorddushaev@gmail.com>
2023-09-20 02:05:52 +0500

    first

---
+++ clock-switch.js
@@ -0,0 +1,86 @@
+
+var clock = +prompt('Write clock!', '');
+
+switch(clock) {
+  case 0:
+    alert('A clock is 24:00 to night');
+    break;
+  case 1:
+    alert('A clock is 1:00');
+    break;
+  case 2:
+    alert('A clock is 2:00');
+    break;
+  case 3:
+    alert('A clock is 3:00');
+    break;
+  case 4:
+    alert('A clock is 4:00');
+    break;
+  case 5:
+    alert('A clock is 5:00');
+    break;
+  case 6:
+    alert('A clock is 6:00');
+    break;
+  case 7:
+    alert('A clock is 7:00');
+    break;
+  case 8:
+    alert('A clock is 8:00');
+    break;
+  case 9:
+    alert('A clock is 9:00');
+    break;
+  case 10:
+    alert('A clock is 10:00');
+    break;
+  case 11:
+    alert('A clock is 11:00');
+    break;
+  case 12:
+    alert('A clock is 12:00');
+    break;
+  case 13:
+    alert('A clock is 13:00');
+    break;
+  case 14:
+    alert('A clock is 14:00');
+    break;
+  case 15:
+    alert('A clock is 15:00');
+    break;
+  case 16:
+    alert('A clock is 16:00');
+    break;
+  case 17:
+    alert('A clock is 17:00');
+    break;
+  case 18:
+    alert('A clock is 18:00');
+    break;
+  case 19:
+    alert('A clock is 19:00');
+    break;
+  case 20:
+    alert('A clock is 20:00');
+    break;
+  case 21:
+    alert('A clock is 21:00');
+    break;
+  case 22:
+    alert('A clock is 22:00');
+    break;
+  case 23:
+    alert('A clock is 23:00');
+    break;
+  case 24:
+    alert('A clock is 24:00');
+    break;
+    default:
+      alert('Something is wrong');
+      break;
+}
+
+
+
